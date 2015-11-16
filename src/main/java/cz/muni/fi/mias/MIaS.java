ea30c337e8ed8e1a2e2768d61c63bc9727095629
martin.liska <martin.liska@ibacz.eu>
2015-11-16 14:48:30 +0100

    Prechod na Apache CLI a cesta ku konfiguracnemu suboru ako command line argument

--- src/main/java/cz/muni/fi/mias/MIaS.java
+++ src/main/java/cz/muni/fi/mias/MIaS.java
@@ -5,6 +5,14 @@ import cz.muni.fi.mias.search.Searching;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 /**
  * Main class witch main method.
@@ -18,95 +26,54 @@ public class MIaS {
      * @param see printHelp()
      */
     public static void main(String[] args) {
-        
-        if (args.length == 0) {
-            printHelp();
-            System.exit(1);
-        }
-
-        if (args[0].equals("-h")||args[0].equals("-help")) {
-            printHelp();
-            System.exit(0);
-        }
-
-        if (args[0].equals("-add")) {
-            if (args.length > 3) {
-                printHelp();
+        Options options = Settings.getMIaSOptions();
+        try {
+            if (args.length == 0) {
+                printHelp(options);
                 System.exit(1);
             }
-            Indexing i = new Indexing();
-            i.indexFiles(args[1], args[2]);
-        }
-        if (args[0].equals("-overwrite")) {
-            if (args.length != 3) {
-                printHelp();
-                System.exit(1);
+            
+            CommandLineParser parser = new DefaultParser();
+            CommandLine cmd = parser.parse(options, args);
+            
+            Settings.init(cmd.getOptionValue(Settings.OPTION_CONF));
+            
+            if (cmd.hasOption(Settings.OPTION_ADD)) {
+                Indexing i = new Indexing();
+                i.indexFiles(cmd.getOptionValues(Settings.OPTION_ADD)[0], cmd.getOptionValues(Settings.OPTION_ADD)[1]);
             }
-            Indexing i = new Indexing();
-            i.deleteIndexDir();
-            i.indexFiles(args[1], args[2]);
-        }
-        if (args[0].equals("-search")) {
-            Searching s = new Searching();
-            String file = null;
-            for (int i = 1; i < args.length; i++) {
-                if (args[i].equals("-file")) {
-                    file = args[i+1];
-                    i++;
-                }
+            if (cmd.hasOption(Settings.OPTION_OVERWRITE)) {
+                Indexing i = new Indexing();
+                i.deleteIndexDir();
+                i.indexFiles(cmd.getOptionValues(Settings.OPTION_OVERWRITE)[0], cmd.getOptionValues(Settings.OPTION_OVERWRITE)[1]);
             }
-            if (file!=null) {
-                try{
-                InputStream is = new FileInputStream(file);
-                s.search(is);
-                } catch (IOException e) {
-                    System.out.println(e.getMessage());
-                    System.exit(1);
-                }
-            } else {
-                s.search(System.in);
+            if (cmd.hasOption(Settings.OPTION_OPTIMIZE)) {
+                Indexing i = new Indexing();
+                i.optimize();
             }
-        }
-        if (args[0].equals("-optimize")) {
-            Indexing i = new Indexing();
-            i.optimize();
-        }
-        if (args[0].equals("-deleteindex")) {
-            Indexing i = new Indexing();
-            i.deleteIndexDir();
-        }
-        if (args[0].equals("-delete")) {
-            if (args.length != 2) {
-                printHelp();
-                System.exit(1);
+            if (cmd.hasOption(Settings.OPTION_DELETEINDEX)) {
+                Indexing i = new Indexing();
+                i.deleteIndexDir();
             }
-            Indexing i = new Indexing();
-            i.deleteFiles(args[1]);
-        }
-        if (args[0].equals("-stats")) {
-            Indexing i = new Indexing();
-            i.getStats();
-        }
-        if (args[0].equals("-indocprocess")) {
-            if (args.length != 3) {
-                printHelp();
-                System.exit(1);
+            if (cmd.hasOption(Settings.OPTION_DELETE)) {
+                Indexing i = new Indexing();
+                i.deleteFiles(cmd.getOptionValue(Settings.OPTION_DELETE));
             }
-            InDocProcessing idp = new InDocProcessing(args[1], args[2]);
-            idp.process();
+            if (cmd.hasOption(Settings.OPTION_STATS)) {
+                Indexing i = new Indexing();
+                i.getStats();
+            }
+            if (cmd.hasOption(Settings.OPTION_INDOCPROCESS)) {
+                InDocProcessing idp = new InDocProcessing(cmd.getOptionValues(Settings.OPTION_INDOCPROCESS)[0], cmd.getOptionValues(Settings.OPTION_INDOCPROCESS)[1]);
+                idp.process();
+            }
+        } catch (ParseException ex) {
+            printHelp(options);
         }
     }
 
-    static void printHelp() {
-        System.out.println("Wrong input. Usage:");
-        System.out.println("MIaS.jar -add <input_path> <root_dir> where root_dir is an absolute path to a directory in the input_path to determine the relative path that the files will be indexed with");
-        System.out.println("MIaS.jar -overwrite <input_path> <root_dir> Overwrites existing index " +
-                "or creates a new one and adds file(s). See -add for parameters");
-        System.out.println("MIaS.jar -search [-file <queries_file>] [-prefix p] Searches the index for a query in file.");
-        System.out.println("MIaS.jar -optimize Optimizes the index for maximum searching performance.");
-        System.out.println("MIaS.jar -deleteindex  Deletes the index.");
-        System.out.println("MIaS.jar -delete <dir_or_file> Deletes file(s) from index.");
-        System.out.println("MIaS.jar -stats Prints statistics about index.");
-        System.out.println("MIaS.jar -indocprocess <input_path> <root_dir> where root_dir is an absolute path to a directory in the input_path. Processes math formulae and inserts M-terms into documents created under root_dir.");
+    static void printHelp(Options options) {        
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp( "MIaS", options );
     }
 }
--- src/main/java/cz/muni/fi/mias/Settings.java
+++ src/main/java/cz/muni/fi/mias/Settings.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
 
 /**
  * Settings class responsible for loading settings from mias.properties Property file.
@@ -19,30 +21,65 @@ public class Settings {
     public static char dirSep = System.getProperty("file.separator").charAt(0);
     public static String eol = System.getProperty("line.separator");
     public static final String MATHDOCHEADER = "<?xml version='1.0' encoding='UTF-8'?><!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1 plus MathML 2.0//EN\" \"http://www.w3.org/TR/MathML2/dtd/xhtml-math11-f.dtd\">";
-
-    static {
+    
+    public static final String OPTION_CONF = "conf";
+    public static final String OPTION_ADD = "add";
+    public static final String OPTION_OVERWRITE = "overwrite";
+    public static final String OPTION_DELETE = "delete";
+    public static final String OPTION_OPTIMIZE = "optimize";
+    public static final String OPTION_DELETEINDEX = "deleteindex";
+    public static final String OPTION_STATS = "stats";
+    public static final String OPTION_INDOCPROCESS = "indocprocess";
+    
+    public static Options getMIaSOptions() {
+        Options options = new Options();
+        options.addOption(Option.builder(OPTION_CONF)
+            .hasArg()
+            .required()
+            .desc("Path to indexing configuration file.")
+            .build());
+        options.addOption(Option.builder(OPTION_ADD)
+            .hasArgs()
+            .numberOfArgs(2)
+            .argName("input_path> <root_dir")
+            .desc("where root_dir is an absolute path to a directory in the input_path to determine the relative path that the files will be indexed with")
+            .build());
+        options.addOption(Option.builder(OPTION_OVERWRITE)
+            .hasArgs()
+            .numberOfArgs(2)
+            .argName("input_path> <root_dir")
+            .desc("Overwrites existing index")
+            .build());
+        options.addOption(Option.builder(OPTION_DELETE)
+            .hasArg()
+            .argName("dir_or_file")
+            .desc("Deletes file(s) from index.")
+            .build());
+        options.addOption(Option.builder(OPTION_OPTIMIZE)
+            .desc("Optimizes the index for maximum searching performance.")
+            .build());
+        options.addOption(Option.builder(OPTION_DELETEINDEX)
+            .desc("Deletes the index.")
+            .build());
+        options.addOption(Option.builder(OPTION_STATS)
+            .desc("Prints statistics about index.")
+            .build());
+        options.addOption(Option.builder(OPTION_INDOCPROCESS)
+            .hasArgs()
+            .numberOfArgs(2)
+            .argName("input_path> <root_dir")
+            .desc("where root_dir is an absolute path to a directory in the input_path. Processes math formulae and inserts M-terms into documents created under root_dir.")
+            .build());
+        return options;
+    }
+    
+    public static void init(String propertiesFilePath) {
         config = new Properties();
-        String path;
-        Properties configFilePointer = new Properties();
         try {
-            configFilePointer.load(Settings.class.getResourceAsStream("mias.to"));
-            path = configFilePointer.getProperty("MIASPATH");
-        } catch (IOException ex) {
-            path = "./mias.properties";
-            Logger.getLogger(Settings.class.getName()).log(Level.WARNING, "Cannot load properties file path from 'mias.to' file. Using {0} instead", path);
-        }
-        try {
-            config.load(new FileInputStream(path));
+            config.load(new FileInputStream(propertiesFilePath));
         } catch (Exception e) {
-            Logger.getLogger(Settings.class.getName()).log(Level.WARNING, "Cannot load properties file in " + path);
-            path = "./mias.properties";
-            Logger.getLogger(Settings.class.getName()).log(Level.INFO, "Using "+path+" instead.");
-            try {
-                config.load(new FileInputStream(path));
-            } catch (IOException ex) {
-                Logger.getLogger(Settings.class.getName()).log(Level.SEVERE, "Cannot load properties file in "+path+", please check path of mias.properties.");
-                System.exit(2);
-            }
+            Logger.getLogger(Settings.class.getName()).log(Level.WARNING, "Cannot load properties file in " + propertiesFilePath);
+            System.exit(2);            
         }
     }
     
